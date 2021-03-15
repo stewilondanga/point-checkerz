cda600f503a160d0ebfee7edd15fbed87628478c
Stewart ilondanga <stilondanga@gmail.com>
2021-03-15 11:57:32 +0300

    add js/scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -1,15 +1,15 @@
 var navigate = (function() {
-	$('.dd').toggle();
-	$('.dd_btn').click(function() {
-		var dataName = $(this).attr('data-name');
-		$('.dd').hide();
-		$('.' + dataName).toggle();
-	});
+  $('.dd').toggle();
+  $('.dd_btn').click(function() {
+    var dataName = $(this).attr('data-name');
+    $('.dd').hide();
+    $('.' + dataName).toggle();
+  });
 })();
 
 console.clear();
-document.getElementById("hand").addEventListener("transitionend", function(){
-    if (document.getElementById("useless").checked == true) {
-        document.getElementById("useless").checked = false;
-    }
+document.getElementById("hand").addEventListener("transitionend", function() {
+  if (document.getElementById("useless").checked == true) {
+    document.getElementById("useless").checked = false;
+  }
 });
